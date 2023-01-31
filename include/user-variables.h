6d065674ad842556ff28c4500d23a8da23d10f71
gemi254 <gemi254@192.168.56.1>
2023-01-31 17:04:57 +0200

    Added combo box for time_zone

--- include/user-variables.h
+++ include/user-variables.h
@@ -133,7 +133,38 @@ const char* appConfigDict_json PROGMEM = R"~(
 },{
      "name": "time_zone",
     "label": "Needs to be a time zone string<br><small>https://raw.githubusercontent.com/nayarsystems/posix_tz_db/master/zones.csv</small>",
-  "default": "EET-2EEST,M3.5.0/3,M10.5.0/4"     
+  "default": "EET-2EEST,M3.5.0/3,M10.5.0/4",    
+ "datalist": "
+'Etc/GMT,GMT0'
+'Etc/GMT-0,GMT0'
+'Etc/GMT-1,<+01>-1'
+'Etc/GMT-2,<+02>-2'
+'Etc/GMT-3,<+03>-3'
+'Etc/GMT-4,<+04>-4'
+'Etc/GMT-5,<+05>-5'
+'Etc/GMT-6,<+06>-6'
+'Etc/GMT-7,<+07>-7'
+'Etc/GMT-8,<+08>-8'
+'Etc/GMT-9,<+09>-9'
+'Etc/GMT-10,<+10>-10'
+'Etc/GMT-11,<+11>-11'
+'Etc/GMT-12,<+12>-12'
+'Etc/GMT-13,<+13>-13'
+'Etc/GMT-14,<+14>-14'
+'Etc/GMT0,GMT0'
+'Etc/GMT+0,GMT0'
+'Etc/GMT+1,<-01>1'
+'Etc/GMT+2,<-02>2'
+'Etc/GMT+3,<-03>3'
+'Etc/GMT+4,<-04>4'
+'Etc/GMT+5,<-05>5'
+'Etc/GMT+6,<-06>6'
+'Etc/GMT+7,<-07>7'
+'Etc/GMT+8,<-08>8'
+'Etc/GMT+9,<-09>9'
+'Etc/GMT+10,<-10>10'
+'Etc/GMT+11,<-11>11'
+'Etc/GMT+12,<-12>12'"
 },{
      "name": "ntp_server",
     "label": "Time server to sync time",
