e1e6e1c71814d61a9117e74a203d041bfc0076a6
gemi254 <gemi254@192.168.56.1>
2023-02-04 14:38:03 +0200

    List directory on spiffs

--- src/files.h
+++ src/files.h
@@ -1,49 +1,50 @@
+
+#define DEBUG_FILES
+#if defined(DEBUG_FILES)
+  #undef LOG_DBG
+  #define LOG_DBG(format, ...) Serial.printf(DBG_FORMAT(format), ##__VA_ARGS__)
+#endif
+
 #define ONEMEG (1024 * 1024)
 #define DATA_DIR "/data" //Dicectory to save data logs
 String readString; // do not change this variable
 
 //Append text at end of a file
 void writeFile(const char * path, const char * message) {
-  LOG_DBG("Writing file: %s", path);
-
   File file = STORAGE.open(path, FILE_APPEND);
   if (!file) {
-    LOG_ERR("\nFailed to open: %s", path);
+    LOG_ERR("\nFailed to open: %s\n", path);
     return;
   }
   if (file.print(message)) {
-    LOG_DBG(" OK\n");
+    LOG_DBG("Writing file: %s OK\n", path);
   } else {
     LOG_ERR("\nFailed to write: %s\n", path);
   }
 }
 
 void listDir(const char * dirname, uint8_t levels) {
-  LOG_INF("Listing directory: %s\r\n", dirname);
+  LOG_INF("Listing directory: %s\n", dirname);
 
   File root = STORAGE.open(dirname);
   if (!root) {
-    LOG_ERR("- failed to open directory");
+    LOG_ERR("- failed to open directory\n");
     return;
   }
   if (!root.isDirectory()) {
-    LOG_ERR(" - not a directory");
+    LOG_ERR(" - not a directory\n");
     return;
   }
 
   File file = root.openNextFile();
   while (file) {
     if (file.isDirectory()) {
-      LOG_INF("  DIR : ");
-      LOG_INF("%s" ,file.name());
+      LOG_INF("  Dir: %s\n", file.name());
       if (levels) {
         listDir(file.name(), levels - 1);
       }
     } else {
-      LOG_INF("  FILE: ");
-      LOG_INF("%s" ,file.name());
-      LOG_INF("\tSIZE: ");
-      LOG_INF("%lu",file.size());
+      LOG_INF("  File: %s, sz: %lu\n", file.path(), file.size());
     }
     file = root.openNextFile();
   }
