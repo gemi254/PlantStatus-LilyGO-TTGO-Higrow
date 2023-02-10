a87726e809b7c340654a2b631d66e1f48c1f6c58
gemi254 <gemi254@192.168.56.1>
2023-02-10 16:32:53 +0200

    Applicaton html to program mem

---
+++ src/appPMem.h
@@ -0,0 +1,315 @@
+//SVG icons used
+PROGMEM const char HTML_PAGE_SVG_SUN[] = R"=====(
+              <svg id="sun" width="40px" height="40px" preserveAspectRatio="xMidYMid meet" focusable="false" role="img" aria-hidden="true" viewBox="0 0 24 24">
+              <g>
+                <path fill="lightskyblue" d="M12,18A6,6 0 0,1 6,12A6,6 0 0,1 12,6A6,6 0 0,1 18,12A6,6 0 0,1 12,18M20,15.31L23.31,12L20,8.69V4H15.31L12,0.69L8.69,4H4V8.69L0.69,12L4,15.31V20H8.69L12,23.31L15.31,20H20V15.31Z"></path>
+              </g>
+              </svg>
+)=====";
+PROGMEM const char HTML_PAGE_SVG_THERMOMETER[] = R"=====(
+              <svg id="thermometer" width="40px" height="40px" preserveAspectRatio="xMidYMid meet" focusable="false" role="img" aria-hidden="true" viewBox="0 0 24 24">
+              <g>
+                <path fill="lightskyblue" d="M15 13V5A3 3 0 0 0 9 5V13A5 5 0 1 0 15 13M12 4A1 1 0 0 1 13 5V8H11V5A1 1 0 0 1 12 4Z"></path>
+              </g>
+              </svg>
+)=====";
+PROGMEM const char HTML_PAGE_SVG_RAIN[] = R"=====(
+              <svg id="rain" width="40px" height="40px" preserveAspectRatio="xMidYMid meet" focusable="false" role="img" aria-hidden="true" viewBox="0 0 24 24">
+              <g>
+                <path fill="lightskyblue" d="M6,14.03A1,1 0 0,1 7,15.03C7,15.58 6.55,16.03 6,16.03C3.24,16.03 1,13.79 1,11.03C1,8.27 3.24,6.03 6,6.03C7,3.68 9.3,2.03 12,2.03C15.43,2.03 18.24,4.69 18.5,8.06L19,8.03A4,4 0 0,1 23,12.03C23,14.23 21.21,16.03 19,16.03H18C17.45,16.03 17,15.58 17,15.03C17,14.47 17.45,14.03 18,14.03H19A2,2 0 0,0 21,12.03A2,2 0 0,0 19,10.03H17V9.03C17,6.27 14.76,4.03 12,4.03C9.5,4.03 7.45,5.84 7.06,8.21C6.73,8.09 6.37,8.03 6,8.03A3,3 0 0,0 3,11.03A3,3 0 0,0 6,14.03M12,14.15C12.18,14.39 12.37,14.66 12.56,14.94C13,15.56 14,17.03 14,18C14,19.11 13.1,20 12,20A2,2 0 0,1 10,18C10,17.03 11,15.56 11.44,14.94C11.63,14.66 11.82,14.4 12,14.15M12,11.03L11.5,11.59C11.5,11.59 10.65,12.55 9.79,13.81C8.93,15.06 8,16.56 8,18A4,4 0 0,0 12,22A4,4 0 0,0 16,18C16,16.56 15.07,15.06 14.21,13.81C13.35,12.55 12.5,11.59 12.5,11.59"></path>
+              </g>
+              </svg>
+)=====";
+PROGMEM const char HTML_PAGE_SVG_PERC_DROP[] = R"=====(
+             <svg id="perc_drop"width="40px" height="40px" preserveAspectRatio="xMidYMid meet" focusable="false" role="img" aria-hidden="true" viewBox="0 0 24 24">
+              <g>
+                <path fill="lightskyblue" d="M12,3.25C12,3.25 6,10 6,14C6,17.32 8.69,20 12,20A6,6 0 0,0 18,14C18,10 12,3.25 12,3.25M14.47,9.97L15.53,11.03L9.53,17.03L8.47,15.97M9.75,10A1.25,1.25 0 0,1 11,11.25A1.25,1.25 0 0,1 9.75,12.5A1.25,1.25 0 0,1 8.5,11.25A1.25,1.25 0 0,1 9.75,10M14.25,14.5A1.25,1.25 0 0,1 15.5,15.75A1.25,1.25 0 0,1 14.25,17A1.25,1.25 0 0,1 13,15.75A1.25,1.25 0 0,1 14.25,14.5Z"></path>
+              </g>
+              </svg>
+)=====";
+PROGMEM const char HTML_PAGE_SVG_EYE[] = R"=====(
+              <svg id="eye" width="40px" height="40px" preserveAspectRatio="xMidYMid meet" focusable="false" role="img" aria-hidden="true" viewBox="0 0 24 24">
+              <g>
+                <path fill="lightskyblue" d="M12,9A3,3 0 0,0 9,12A3,3 0 0,0 12,15A3,3 0 0,0 15,12A3,3 0 0,0 12,9M12,17A5,5 0 0,1 7,12A5,5 0 0,1 12,7A5,5 0 0,1 17,12A5,5 0 0,1 12,17M12,4.5C7,4.5 2.73,7.61 1,12C2.73,16.39 7,19.5 12,19.5C17,19.5 21.27,16.39 23,12C21.27,7.61 17,4.5 12,4.5Z"></path>
+              </g>
+              </svg>
+)=====";
+PROGMEM const char HTML_PAGE_SVG_CLOCK[] = R"=====(
+              <svg width="40px" height="40px" id="clock" preserveAspectRatio="xMidYMid meet" focusable="false" role="img" aria-hidden="true" viewBox="0 0 24 24">
+              <g>
+                <path fill="lightskyblue" d="M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M16.2,16.2L11,13V7H12.5V12.2L17,14.9L16.2,16.2Z"></path>
+              </g>
+              </svg>
+)=====";
+// Template for header, begin of the config form
+PROGMEM const char HTML_PAGE_HOME_START[] = 
+R"=====(<!DOCTYPE HTML>
+<html lang='de'>
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<title>Sensors of {host_name}</title>
+<style>
+:root {
+ --bg-table-stripe: #f6f6f5;
+ --b-table: #999a9b47;
+ --caption: cornflowerblue;
+}
+body {
+	font-family: Arial, Helvetica, sans-serif;
+}
+.column {
+	float: center;
+	width: 100%;
+	padding: 0 10px;
+	box-sizing: border-box;
+  margin-top: 10px;
+}
+.row {
+	margin: 0 -5px;
+}
+/* Clear floats after the columns */
+.row:after {
+	content: "";
+	display: table;
+	clear: both;
+}
+/* Responsive columns */
+@media screen and (max-width: 600px) {
+.column {
+	width: 100%;
+	display: block;
+	margin-bottom: 20px;
+  margin-top: 2px;
+}
+}
+.column .card.closed{
+    height: 30px;
+    overflow: hidden;
+}
+.card {
+	box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2);
+	padding: 10px;
+	text-align: center;
+	background-color: #ffffff;
+	margin-bottom: 15px;
+	border-radius: 5px;
+}
+.card h3{
+	margin:2px;
+}
+.card h2{
+	margin-top:2px;
+	margin-bottom:8px;
+	color: var(--caption);
+}
+.container {
+	display: flex;
+	justify-content: center;
+	flex-direction: row;
+}
+table {
+	width: 100%;
+}
+table th, table td {
+	padding: .325em;
+	border: 1px solid var(--b-table);
+}
+tbody tr:nth-of-type(2n+1) {
+	background-color: var(--bg-table-stripe)
+}
+.card-key {
+	text-align: right;
+}
+.card-val {
+	text-align: left;
+	font-weight: 800;
+  color: midnightblue;
+  font-size: 1.2em;
+}
+.card-lbl {
+	text-align: left;
+	font-style: italic;
+}
+.card-icon {
+	width: 70px;
+}
+button {
+	background-color: #fff;
+	border: 1px solid #d5d9d9;
+	border-radius: 4px;
+	box-shadow: rgb(213 217 217 / 50%) 0 2px 5px 0;
+	box-sizing: border-box;
+	font-weight: 900;
+	color: #0f1111;
+	cursor: pointer;
+	display: inline-block;
+	padding: 0 10px 0 11px;
+	margin: 2px;
+	line-height: 29px;
+	position: relative;
+	text-align: center;
+	vertical-align: middle;
+}
+button:hover {
+	background-color: #f7fafa;
+}
+</style>
+</head> )=====";
+//Scripts to handle websockets
+PROGMEM const char HTML_PAGE_HOME_SCRIPT[] = R"=====(
+<script>
+/*********** WebSockets functions ***********/
+const wsServer = "ws://" + document.location.host + ":81/";
+let ws = null;
+let hbTimer = null;
+let refreshInterval = 10000;
+document.addEventListener('DOMContentLoaded', initWebSocket());
+
+// define websocket handling
+function initWebSocket() {  
+  console.log("Connect to: " + wsServer);
+  ws = new WebSocket(wsServer);
+  ws.onopen = onOpen;
+  ws.onclose = onClose;
+  ws.onmessage = onMessage; 
+  ws.onerror = onError;
+}
+
+function sendCmd(reqStr) {
+  ws.send(reqStr);
+  console.log("Cmd: " + reqStr);
+}
+
+// periodically check that connection is still up and get status
+function heartbeat() {
+  if (!ws) return;
+  if (ws.readyState !== 1) return;
+  sendCmd("H");
+  clearTimeout(hbTimer);
+  hbTimer = setTimeout(heartbeat, refreshInterval);
+}
+
+// connect to websocket server
+function onOpen(event) {
+  console.log("Connected");
+  heartbeat();
+}
+
+// process received WS message
+function onMessage(messageEvent) {
+  if (messageEvent.data.startsWith("{")) {
+    // json data
+    updateData = JSON.parse(messageEvent.data);
+    updateTable(updateData); // format received config json into html table
+  } else {
+    console.log(messageEvent.data);
+  }
+
+  if(hbTimer){
+    clearTimeout(hbTimer);
+    hbTimer = setTimeout(heartbeat, refreshInterval);
+  }
+}
+
+function updateTable(configData){
+  Object.entries(configData).forEach(([key, value]) => {    
+    const td = document.getElementById(key)
+    if(td){
+      if(key!="time" && key!="batChargeDate" && key!="onPower" && key!="sleepReason"){
+        const inc = document.getElementById(key+"_inc");
+        if(td.innerHTML == value) inc.innerHTML = "";
+        else if(td.innerHTML < value) { inc.style.color="lightgreen"; inc.innerHTML = "▲"; }
+        else { inc.style.color="lightcoral"; inc.innerHTML = "▼"; }
+      }
+      td.innerHTML = value;
+    } 
+  });
+}
+
+function onError(event) {
+  console.log("WS Error: " + event.code);
+}
+
+function onClose(event) {
+  console.log("Disconnected: " + event.code + ' - ' + event.reason);
+  ws = null;
+  // event.codes:
+  //   1006 if server not available, or another web page is already open
+  //   1005 if closed from app
+  if (event.code == 1006) {} //alert("Closed websocket as a newer connection was made, refresh browser page");
+  else if (event.code != 1005) initWebSocket(); // retry if any other reason
+}
+</script>  )=====";
+
+// Template html body
+PROGMEM const char HTML_PAGE_HOME_BODY[] = R"=====(
+<body>
+<div class="container">
+  <div class="row">
+    <div class="column">
+      <div class="card">
+        <h3><font style="color: darkblue;">{host_name}</font> status</h3>
+      </div>
+      <div class="card">
+        <table>
+          <h2><font style="color: mediumblue;">{plant_name}</font> sensors</h2>
+)=====";
+// Template for one line
+PROGMEM const char HTML_PAGE_HOME_LINE[] = R"=====(
+          <tr>
+            <td class="card-icon">{svg}</td>
+            <td class="card-key">{name}</td>
+            <td class="card-val"><span id="{key}">{val}</span>&nbsp;<font color='lightskyblue'>{units}</font>&nbsp;<span id="{key}_inc"></span></td>
+          </tr>
+)=====";
+PROGMEM const char HTML_PAGE_HOME_SEP[] = R"=====(
+        </table> 
+      </div>
+      <div class="card closed" onClick="this.classList.toggle('closed')">
+        <h2>Device Information</h2>
+        <table>     
+)=====";
+// Template for page end
+PROGMEM const char HTML_PAGE_HOME_END[] = R"=====(
+      </table>
+      </div>
+      <div class="card">
+       <table>
+            <tr>
+              <td style="text-align: center;" colspan="5">
+              <button title='View sensors daily log' onClick='window.location.href="/cmd?view="'>Day</button>
+              <button title='View sensors monthly logs' onClick='window.location.href="/fs?dir=/data"'>Logs</button>
+              <button title='Send device mqtt discovery message to Homeassistant' onClick='window.location.href="/cmd?hasDiscovery=1"'>Discover</button>
+              <button title='Configure this device' onClick='window.location.href="/cfg"'>Configure</button>
+              <button title='Reboot device' onClick='window.location.href="/cmd?reboot=1"'>Reboot</button>
+              <button title='Factory defaults' onClick='window.location.href="/cmd?reset=1"'>Reset</button>
+          </tr>
+        </table>
+      </div>
+    </div>
+  </br>
+  <center><small>PlantStatus Ver: {appVer}</small></center>
+  </div>
+</div>
+</body>
+</html>
+)=====";
+
+//Back button
+PROGMEM const char HTML_BACK_SCRIPT[] = R"~(
+<small><a href="" onClick="window.history.go(-1); return false;">[&nbsp;Back&nbsp;]</a></small>
+)~";
+
+// Ping script
+PROGMEM const char HTML_PING_SCRIPT[] = R"~(
+<script>
+function ping(){
+  const Http = new XMLHttpRequest();
+  const url='/pg';
+  Http.open("GET", url);
+  Http.send();
+}
+setInterval(ping, 10000);
+</script>
+)~";
