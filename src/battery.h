d9fb432918ce6b204fc91537d46f88c5839a9ebe
gemi254 <gemi254@192.168.56.1>
2023-02-24 10:11:08 +0200

    Calc bat percent avg

--- src/battery.h
+++ src/battery.h
@@ -7,21 +7,27 @@ float calcBattery(uint16_t AdcVolt){
 
   data.batAdcVolt = AdcVolt;
   // int vref = 1100;
-  // float battery_voltage = ((float)volt / 4095.0) * 2.0 * 3.3 * (vref) / 1000;
-  // float battery_voltage =  0.001338217338 * volt +  0.948424908425 ;//volts
   float bat_volt_low = atof(conf["bat_reading_low"].c_str());
   float bat_volt_high = atof(conf["bat_reading_high"].c_str());
   float battery_voltage =  ( bat_volt_high - bat_volt_low ) / ( bat_reading_high - bat_reading_low ) * AdcVolt
         +  bat_volt_high - bat_reading_high * ( bat_volt_high - bat_volt_low ) / ( bat_reading_high - bat_reading_low ) ;//true volts
 
   data.batVolt = battery_voltage;
-  //Battery volt percents
+  
+  //Battery volt percent
   float batPerc = 100.0F * ( battery_voltage - bat_volt_low ) / ( bat_volt_high - bat_volt_low);
-  batPerc = truncateFloat(batPerc, 1);
+  //Average from previous value
+  float batPercAgv = batPerc;
+  if(lastBoot["bat_perc"] !="" ){
+     float lastPerc = (atof(lastBoot["bat_perc"].c_str()));
+     batPercAgv = ((lastPerc + truncateFloat(batPerc, 0) ) / 2.0F);
+  }  
+  LOG_DBG("Battery last perc: %s, cur: %3.1f, avg: %3.1f\n", lastBoot["bat_perc"].c_str(), batPerc, batPercAgv);
+  
   if (batPerc > BATT_PERC_ONPOWER) onPower = true;
   else onPower = false;    
-  LOG_INF("Battery adcV: %lu, V: %3.3f, perc: %3.1f %%, onPower: %i\n", AdcVolt, battery_voltage, batPerc, onPower);
-  return batPerc;
+  LOG_INF("Battery adcV: %lu, V: %3.3f, perc: %3.1f %%, onPower: %i\n", AdcVolt, battery_voltage, batPercAgv, onPower);
+  return batPercAgv;
   
 }
 // Calculate battery discharge days
@@ -32,7 +38,10 @@ float calcBatteryDays(){
     //Is time synced?
     if(curDate.length()>0){
       data.batChargeDate = curDate;
-      lastBoot.put("bat_charge_date", data.batChargeDate,true);
+      File f = STORAGE.open(LAST_BAT_INF, "w");
+      f.print(data.batChargeDate);
+      f.close();
+      //lastBoot.put("bat_charge_date", data.batChargeDate, true);
     }
     //Reset counters
     lastBoot.put("boot_cnt", "0",true);
@@ -40,16 +49,21 @@ float calcBatteryDays(){
     LOG_INF("Battery CHARGING, date: %s\n", curDate.c_str());    
   }else{ //Discharging
     onPower = false;
-    data.batChargeDate = lastBoot["bat_charge_date"];
-    time_t tmStart = convertDateTimeString(data.batChargeDate);
-    time_t tmNow = getEpoch();
-    if(tmStart > 10000L && tmNow > 10000L){
-      daysOnBattery  = (tmNow - tmStart) / BATT_CHARGE_DATE_DIVIDER;
-      if(daysOnBattery < 0.0F)  daysOnBattery = 0.0F;      
-    }else{
-      daysOnBattery = 0.0F;
+    daysOnBattery = 0.0F;
+    File f = STORAGE.open(LAST_BAT_INF,"r");
+    if(f){
+      //data.batChargeDate = lastBoot["bat_charge_date"];
+      data.batChargeDate = f.readString();
+      f.close();
+      time_t tmStart = convertDateTimeString(data.batChargeDate);
+      time_t tmNow = getEpoch();
+      if(tmStart > 10000L && tmNow > 10000L){
+        daysOnBattery  = (tmNow - tmStart) / BATT_CHARGE_DATE_DIVIDER;
+        if(daysOnBattery < 0.0F)  daysOnBattery = 0.0F;      
+      }
+      //LOG_INF("Battery DISCHARGING, st: %lu, now: %lu\n", tmStart, tmNow);
     }
-    LOG_INF("Battery DISCHARGING, st: %lu, now: %lu, date: %s, days: %5.3f\n", tmStart, tmNow, data.batChargeDate.c_str(), daysOnBattery);
+    LOG_INF("Battery DISCHARGING, date: %s, days: %5.3f\n", data.batChargeDate.c_str(), daysOnBattery);
   }
   
   return daysOnBattery;
