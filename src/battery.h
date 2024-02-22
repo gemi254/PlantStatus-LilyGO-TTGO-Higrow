be61ebcccd7846c249284eb9d310c6556d058ad8
gemi254 <gemi254@192.168.56.1>
2024-02-22 15:54:26 +0200

    Remove spaces

--- src/battery.h
+++ src/battery.h
@@ -1,6 +1,6 @@
 // Calc battery state. Higrow uses TP4054 linear charger
 uint32_t readBatteryADC(){
-  uint32_t adc_reading = 0;    
+  uint32_t adc_reading = 0;
     for (int i = 0; i < BATT_NO_OF_SAMPLES; i++) {
         adc_reading += analogRead(BAT_ADC);
         delay(2);
@@ -11,14 +11,14 @@ uint32_t readBatteryADC(){
 
 float calcBattery(uint16_t AdcVolt){
   data.batAdcVolt = AdcVolt;
-  
+
   // Battery adc volt measured with multimetr, after disconecting from board,
   // and value reported at end discharge, and value reported just after charging
 
   //Battery adc readings limits
   float bat_reading_low = atof(conf["bat_adc_low"].c_str());
   float bat_reading_high = atof(conf["bat_adc_high"].c_str());
-  
+
   //Battery voltage limits
   float bat_volt_low = atof(conf["bat_reading_low"].c_str());
   float bat_volt_high = atof(conf["bat_reading_high"].c_str());
@@ -26,7 +26,7 @@ float calcBattery(uint16_t AdcVolt){
   float battery_voltage =  ( bat_volt_high - bat_volt_low ) / ( bat_reading_high - bat_reading_low ) * AdcVolt
         +  bat_volt_high - bat_reading_high * ( bat_volt_high - bat_volt_low ) / ( bat_reading_high - bat_reading_low ) ;//true volts
 
-  data.batVolt = battery_voltage;  
+  data.batVolt = battery_voltage;
   //Battery volt percent
   float batPerc = 100.0F * ( battery_voltage - bat_volt_low ) / ( bat_volt_high - bat_volt_low);
   //Average from previous value
@@ -34,20 +34,20 @@ float calcBattery(uint16_t AdcVolt){
   if(lastBoot["bat_perc"] !="" && lastBoot["bat_perc"] !="nan"){
      float lastPerc = (atof(lastBoot["bat_perc"].c_str()));
      batPercAgv = ((lastPerc + truncateFloat(batPerc, 0) ) / 2.0F);
-  }  
+  }
   LOG_D("Battery last perc: %s, cur: %3.1f, avg: %3.1f\n", lastBoot["bat_perc"].c_str(), batPerc, batPercAgv);
-  
+
   if (batPerc > BATT_PERC_ONPOWER) onPower = true;
-  else onPower = false;    
+  else onPower = false;
   LOG_I("Battery adcV: %lu, V: %3.3f, perc: %3.1f %%, onPower: %i\n", AdcVolt, battery_voltage, batPercAgv, onPower);
   return batPercAgv;
-  
+
 }
 // Calculate battery discharge days
 // Connected to the Internet about 150mA of current, the peak value 300mA, and the average is about 100mA
-float calcBatteryDays(){ 
+float calcBatteryDays(){
   float daysOnBattery = 0.0F;
-  if (onPower){    
+  if (onPower){
     String curDate = getCurDateTimeString();
     //Is time synced?
     if(curDate.length()>0){
@@ -59,7 +59,7 @@ float calcBatteryDays(){
     //Reset counters
     lastBoot.put("boot_cnt", "0",true);
     lastBoot.put("boot_cnt_err", "0",true);
-    LOG_I("Battery CHARGING, date: %s\n", curDate.c_str());    
+    LOG_I("Battery CHARGING, date: %s\n", curDate.c_str());
   }else{ //Discharging
     onPower = false;
     daysOnBattery = 0.0F;
@@ -71,12 +71,12 @@ float calcBatteryDays(){
       time_t tmNow = getEpoch();
       if(tmStart > 10000L && tmNow > 10000L){
         daysOnBattery  = (tmNow - tmStart) / BATT_CHARGE_DATE_DIVIDER;
-        if(daysOnBattery < 0.0F)  daysOnBattery = 0.0F;      
+        if(daysOnBattery < 0.0F)  daysOnBattery = 0.0F;
       }
       //LOG_I("Battery DISCHARGING, st: %lu, now: %lu\n", tmStart, tmNow);
     }
     LOG_I("Battery DISCHARGING, date: %s, days: %5.3f\n", data.batChargeDate.c_str(), daysOnBattery);
   }
-  
+
   return daysOnBattery;
 }
